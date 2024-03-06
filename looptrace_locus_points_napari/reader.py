979d8bb8c03881595e498ece511a8e3fb0169bbb
Vince Reuter <vince.reuter@gmail.com>
2024-03-06 19:41:04 +0100

    working version with first-pass at labels

--- looptrace_locus_points_napari/reader.py
+++ looptrace_locus_points_napari/reader.py
@@ -1,6 +1,7 @@
 """The main functionality of this napari plugin"""
 
 import csv
+import logging
 import os
 from pathlib import Path
 from typing import *
@@ -21,8 +22,8 @@ FailCodesText = str
 PointRecord = Tuple[PointId, Point3D]
 QCPassRecord = PointRecord
 QCFailRecord = Tuple[PointId, Point3D, FailCodesText]
-RawDataLike = List[Union[TraceId, Timepoint, float]]
-FullLayerData = Tuple[RawDataLike, LayerParams, LayerTypeName]
+RawPointLike = Union[TraceId, Timepoint, float]
+FullLayerData = Tuple[List[RawPointLike], LayerParams, LayerTypeName]
 
 QC_FAIL_CODES_KEY = "failCodes"
 
@@ -40,42 +41,53 @@ def get_reader(path: Union[PathLike, List[PathLike]]) -> Optional[Callable[[Path
     If the plugin represented by this package is intended to read a file of the given type (inferred from 
     the file's extension), then the function with which to parse that file. Otherwise, nothing.
     """
-    static_params = {"size": 0.5, "edge_width": 0.1, "edge_width_is_relative": True, "n_dimensional": False}
+    static_params = {"size": 0.5, "edge_width": 0.05, "edge_width_is_relative": True, "n_dimensional": False}
 
     if isinstance(path, (str, Path)):
-        base, ext = os.path.splitext(os.path.basename(p))
+        base, ext = os.path.splitext(os.path.basename(path))
         if ext == ".csv":
-            status_name = base.lower().lstrip("qc_").lstrip("qc")
+            status_name = base.lower().split(".")[-1].lstrip("qc_").lstrip("qc")
             if status_name in {"pass", "passed"}:
+                logging.debug(f"Parsing as QC-pass: {path}")
                 color = "red"
                 symbol = "*"
                 read_rows = parse_passed
             elif status_name in {"fail", "failed"}:
+                logging.debug(f"Parsing as QC-fail: {path}")
                 color = "blue"
                 symbol = "o"
-                read_rows = parse_passed
+                read_rows = parse_failed
             else:
+                logging.warning(f"Could not infer QC status from '{status_name}', from path {path}")
                 return None
             base_meta = {"edge_color": color, "face_color": color, "symbol": symbol}
             def parse(p):
                 with open(p, mode='r', newline='') as fh:
                     rows = list(csv.reader(fh))
                 data, extra_meta = read_rows(rows)
-                return data, {**static_params, **base_meta, **extra_meta}
+                params = {**static_params, **base_meta, **extra_meta}
+                return data, params, "points"
             return lambda p: [parse(p)]
+        else:
+            logging.debug(f"Not a CSV: {path}")
+            return None
+    else:
+        logging.debug(f"Not a path-like: {path}")
+        return None
+            
 
 
-def parse_passed(rows: List[CsvRow]) -> Tuple[RawDataLike, LayerParams]:
+def parse_passed(rows: List[CsvRow]) -> Tuple[List[RawPointLike], LayerParams]:
     return [parse_simple_record(r, exp_len=5) for r in rows], {}
 
 
-def parse_failed(rows: List[CsvRow]) -> Tuple[RawDataLike, LayerParams]:
+def parse_failed(rows: List[CsvRow]) -> Tuple[List[RawPointLike], LayerParams]:
     data_codes_pairs = [(parse_simple_record(r, exp_len=6), r[5]) for r in rows]
     try:
         data, codes = zip(*data_codes_pairs)
     except:
-        data, codes = [], []
-    return data, {"text": QC_FAIL_CODES_KEY, "properties": {QC_FAIL_CODES_KEY: codes}}
+        data, codes = tuple(), tuple()
+    return list(data), {"text": QC_FAIL_CODES_KEY, "properties": {QC_FAIL_CODES_KEY: list(codes)}}
 
 
 def parse_simple_record(r: CsvRow, *, exp_len: int) -> PointRecord:
@@ -89,4 +101,4 @@ def parse_simple_record(r: CsvRow, *, exp_len: int) -> PointRecord:
     z = float(r[2])
     y = float(r[3])
     x = float(r[4])
-    return (trace, timepoint), (z, y, x)
+    return [trace, timepoint, z, y, x]
