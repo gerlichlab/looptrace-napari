5524c2e53ab390383b30cae100994fcffb7196cc
Vince Reuter <vince.reuter@gmail.com>
2024-04-15 12:54:55 +0200

    close #9 and prep release of 0.3.0

--- looptrace_napari/geometry.py
+++ looptrace_napari/geometry.py
@@ -2,13 +2,12 @@
 
 from abc import abstractmethod
 from dataclasses import dataclass
-from typing import Protocol
+from typing import Protocol, Union
 
+import numpy as np
 from numpydoc_decorator import doc  # type: ignore[import-untyped]
 
-# TODO: need Python >= 3.12
-# See: https://github.com/gerlichlab/looptrace-napari/issues/6
-# from typing import override
+ZCoordinate = Union[int, float, np.float64]  # int to accommodate notion of "z-slice"
 
 
 class LocatableXY(Protocol):
@@ -20,6 +19,14 @@ class LocatableXY(Protocol):
 
     @abstractmethod
     def get_y_coordinate(self) -> float:
+        raise NotImplementedError
+
+
+class LocatableZ(Protocol):
+    """Something that admits z-coordinate."""
+
+    @abstractmethod
+    def get_z_coordinate(self) -> ZCoordinate:
         raise NotImplementedError
 
 
@@ -41,13 +48,34 @@ class ImagePoint2D(LocatableXY):
         if any(c < 0 for c in [self.x, self.y]):
             raise ValueError(f"At least one coordinate is negative! {self}")
 
-    # TODO: adopt @override once on Python >= 3.12
-    # See: https://github.com/gerlichlab/looptrace-napari/issues/6
     def get_x_coordinate(self) -> float:
         return self.x
 
-    # TODO: adopt @override once on Python >= 3.12
-    # See: https://github.com/gerlichlab/looptrace-napari/issues/6
-    # @override
     def get_y_coordinate(self) -> float:
         return self.y
+
+
+@doc(
+    summary="Bundle x and y position to create point in 2D space.",
+    parameters=dict(
+        x="Position in x",
+        y="Position in y",
+        z="Position in z",
+    ),
+    see_also=dict(
+        ImagePoint2D="Simpler, non-z implementation of an image point",
+    ),
+)
+@dataclass(kw_only=True, frozen=True)
+class ImagePoint3D(ImagePoint2D, LocatableZ):
+    z: ZCoordinate
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+        if not isinstance(self.z, (int, float, np.float64)):
+            raise TypeError(f"Bad z ({type(self.z).__name__}: {self.z}")
+        if any(c < 0 for c in [self.x, self.y]):
+            raise ValueError(f"z-coordinate is negative! {self}")
+
+    def get_z_coordinate(self) -> ZCoordinate:
+        return self.z
